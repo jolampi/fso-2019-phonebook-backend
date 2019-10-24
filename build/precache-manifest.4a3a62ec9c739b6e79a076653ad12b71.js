237f2fd780b467759cad2efd8cd97b7b97623c0a
jolampi <36279434+jolampi@users.noreply.github.com>
2019-10-25 02:25:16 +0300

    uibuild

---
+++ build/precache-manifest.4a3a62ec9c739b6e79a076653ad12b71.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c445c6a8e03314662925db4b327bc0bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30f3ea4c767310245f16",
+    "url": "/static/js/2.4e8181f6.chunk.js"
+  },
+  {
+    "revision": "f179c907edc9db9c2b2d",
+    "url": "/static/js/main.06f9a843.chunk.js"
+  },
+  {
+    "revision": "a4e9cb56398c85fd6c45",
+    "url": "/static/js/runtime-main.33bda74f.js"
+  }
+]);
